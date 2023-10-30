9184668e1b0813dd1f1bc98d6978cb6fff6975a1
nitaotao <1914866205@qq.com>
2023-10-30 09:57:46 +0800

    调整表格列顺序

--- src/main/java/com/zpmc/analysis/service/impl/ResumeServiceImpl.java
+++ src/main/java/com/zpmc/analysis/service/impl/ResumeServiceImpl.java
@@ -75,7 +75,7 @@ public class ResumeServiceImpl implements ResumeService {
         for (int i = 0; i < chars.length; i++) {
             if (chars[i] <= '9') {
                 targetPath.append(chars[i]);
-            }else{
+            } else {
                 targetPath.append("\\");
                 targetPath.append(path.substring(i));
                 break;
@@ -188,26 +188,34 @@ public class ResumeServiceImpl implements ResumeService {
     public static String getInfo(String partName, String filePath, Row row, Cell cell) throws IOException {
         //调用百度OCR
         String result = OCR.getResult(filePath);
+        //部门名称
         cell = row.createCell(0);
         cell.setCellValue(partName);
-
+        //姓名
         cell = row.createCell(1);
         cell.setCellValue(getName(result));
+        //性别
         cell = row.createCell(2);
         cell.setCellValue(getSex(result));
+        //手机号
         cell = row.createCell(3);
+        cell.setCellValue(getPhoneNumber(result));
+        //学校
+        cell = row.createCell(4);
         Set school = getSchool(result);
         cell.setCellValue(String.valueOf(school).substring(1, String.valueOf(school).length() - 1));
-        cell = row.createCell(4);
-        String sc = checkSchool(school);
-        cell.setCellValue(sc != null ? sc : "不在");
+        //最高学历
         cell = row.createCell(5);
         cell.setCellValue(findHighestEducationLevel(result));
+        //专业
         cell = row.createCell(6);
         Set speciality = getSpeciality(result);
         cell.setCellValue(String.valueOf(speciality).substring(1, String.valueOf(speciality).length() - 1));
+        //检查院校
         cell = row.createCell(7);
-        cell.setCellValue(getPhoneNumber(result));
+        String sc = checkSchool(school);
+        cell.setCellValue(sc != null ? sc : "不在");
+        //邮箱
         cell = row.createCell(8);
         cell.setCellValue(getEmail(result));
         String name = getName(result);
@@ -254,7 +262,7 @@ public class ResumeServiceImpl implements ResumeService {
         }
 
         //一般来说，姓名会出现在前几行
-        String namePre = "李王张刘陈杨赵黄周吴徐孙胡朱高林何郭马罗梁宋郑谢韩唐冯于董萧曹袁邓许傅沈曾彭吕苏卢蒋蔡贾丁魏薛叶阎余潘杜戴夏钟汪田任姜范石姚谭廖邹熊陆郝孔白崔康毛邱秦史顾侯邵孟龙万段漕钱汤尹黎易常武乔贺赖龚文庞樊兰殷施陶洪翟安颜倪严牛温芦季俞章鲁葛伍韦申尤毕聂丛焦向柳邢路岳齐沿梅莫庄辛祝左涂谷祁时舒耿牟卜路詹关苗凌费纪靳盛童欧甄项曲成游阳裴席卫查屈鲍位覃霍翁隋植甘薄单包司柏宁柯阮桂闵欧阳解强柴华车冉房边辜吉饶刁瞿戚丘古米池滕晋苑邬臧畅宫来嵺苟全褚廉娄盖符奚木穆燕郎邸冀谈姬屠连郜晏栾郁商蒙喻揭窦迟宇敖糜鄢冷卓花仇艾蓝都巩稽井练仲乐虞卞封竺冼原官衣楚佟栗匡宗台巫鞠僧桑荆谌银扬明沙薄伏岑习胥保和蔺";
+        String namePre = "李王张刘陈杨赵黄周吴徐孙胡朱高林何郭马罗梁宋郑谢韩唐冯于董萧曹袁邓许傅沈曾彭吕苏卢蒋蔡贾丁魏薛叶阎余潘杜戴夏钟汪田任姜范石姚谭廖邹熊陆郝孔白崔康毛邱秦史顾侯邵孟龙万段漕钱汤尹黎易常武乔贺赖龚文庞樊兰殷施陶洪翟安颜倪严牛温芦季俞章鲁葛伍韦申尤毕聂丛焦向柳邢路岳齐沿梅莫庄辛祝左涂谷祁时舒耿牟卜路詹关苗凌费纪靳盛童欧甄项曲成游阳裴席卫查屈鲍位覃霍翁隋植甘薄单包司柏宁柯阮桂闵欧阳解强柴华车冉房边辜吉饶刁瞿戚丘古米池滕晋苑邬臧畅宫来嵺苟全褚廉娄盖符奚木穆燕郎邸冀谈姬屠连郜晏栾郁商蒙喻揭窦迟宇敖糜鄢冷卓花仇艾蓝都巩稽井练仲乐虞卞封竺冼官衣楚佟栗匡宗台巫鞠僧桑荆谌银扬明沙薄伏岑习胥保和蔺";
         // 定义正则表达式来匹配包含姓名的行，一般来说前一百个字符里就有姓名
         String newRes = result;
         char[] chars = newRes.toCharArray();
@@ -390,5 +398,9 @@ public class ResumeServiceImpl implements ResumeService {
             }
         }
         return null;
+    }
+
+    public static void main(String[] args) throws IOException {
+        analysis("C:\\Users\\nitaotao\\Desktop\\root");
     }
 }
